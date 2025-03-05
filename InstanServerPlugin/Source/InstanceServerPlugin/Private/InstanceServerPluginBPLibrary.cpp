d476457a4f42888397163a3f4464c5b9e870383e
SoulofAO <SoulofAO@mail.com>
2025-03-05 16:09:18 +0400

    Update

---
+++ InstanServerPlugin/Source/InstanceServerPlugin/Private/InstanceServerPluginBPLibrary.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "InstanceServerPluginBPLibrary.h"
+#include "InstanceServerPlugin.h"
+
+UInstanceServerPluginBPLibrary::UInstanceServerPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
